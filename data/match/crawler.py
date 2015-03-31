21651e7698ab03f57efe0ef52caa923392d116d4
lordwarlock <zheng@ZhihaoZhengdeMacBook-Pro.local>
2015-03-30 20:42:13 -0400

    data_crawler_start

---
+++ data/match/crawler.py
@@ -0,0 +1,15 @@
+class match():
+    def __init__()
+
+class BingCrawler():
+    def __init__(self,
+                 pattern = '"http://www1.skysports.com/football/live/match/.*?/report"'),
+                 query_dir_pattern = './match_list/*.csv')
+
+    def readMatchData(self,query_dir = './match_list/E0.csv'):
+        with open(query_dir,'r') as f_csv:
+            _ = f_csv.readline()
+            for line in f_csv:
+                splitted = line.split(',')
+                date = 
+        
